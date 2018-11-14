1df8447e9922e7984154aef0af721755876ceeb7
Peter <pjsheeha@andrew.cmu.edu>
2018-11-14 18:35:03 -0500

    controlsAdded
    Controls to orb added

---
+++ Assets/Script/GravManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class GravManager : MonoBehaviour
+    {
+    public GameObject playerPlanet;
+        public float grav = 9;
+    public bool playerGravOn = false;
+        public float backUpGrav;
+    public GameObject cameraCube;
+    public GameObject grava;
+    //public bool gravOn = false;
+        // Use this for initialization
+        void Start()
+        {
+            backUpGrav = grav;
+
+        }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+        // GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+
+
+                
+
+
+                
+
+
+
+                if (playerGravOn == true)
+                    {
+
+                        grav = playerPlanet.GetComponent<gravPlanetInfo>().gravHere;
+                Vector3 downVec = playerPlanet.transform.position - cameraCube.transform.position;
+                Vector3 bodyUp = cameraCube.transform.up;
+
+
+                Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, downVec / grav) * cameraCube.transform.rotation;
+                cameraCube.transform.rotation = Quaternion.Slerp(cameraCube.transform.rotation, targetRotation, 2 * Time.deltaTime);
+
+                    }
+
+                
+
+
+            }
+        }
+    
+
+
+   
---
+++ Assets/Script/assignedGravPlanet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignedGravPlanet : MonoBehaviour {
+    public GameObject planet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "playerCube" || other.tag == "playerSphere"){
+            GameObject.Find("EventManager").GetComponent<GravManager>().playerPlanet = planet;
+        }
+    }
+}
