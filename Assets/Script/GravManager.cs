7ad4e8ac7333da9f1c366226f92e7719382cdf69
Peter <pjsheeha@andrew.cmu.edu>
2018-11-16 00:28:18 -0500

    finisheddemo

--- Assets/Script/GravManager.cs
+++ Assets/Script/GravManager.cs
@@ -51,27 +51,52 @@ using UnityEngine;
 
                
                     //grav = 1;
-                Vector3 downVec = playerPlanet.transform.position - hasGravity[i].transform.position;
-
-                float forceMagnitude = multiplier * grav / downVec.sqrMagnitude;
-                Vector3 forceVector = downVec.normalized;
-
-
-                Vector3 bodyUp = hasGravity[i].transform.up;
-
-
-                Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, forceMagnitude * forceVector) * hasGravity[i].transform.rotation;
-                hasGravity[i].transform.rotation = Quaternion.Slerp(hasGravity[i].transform.rotation, targetRotation, 2 * Time.deltaTime);
 
 
                 if (hasGravity[i].name != "CameraCube")
                 {
+
+
+                    Vector3 downVec = playerPlanet.transform.position - hasGravity[i].transform.position;
+
+                    float forceMagnitude = multiplier * grav / downVec.sqrMagnitude;
+                    Vector3 forceVector = downVec.normalized;
+
+
+                    Vector3 bodyUp = hasGravity[i].transform.up;
+
+
+                    Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, forceMagnitude * forceVector) * hasGravity[i].transform.rotation;
+                    hasGravity[i].transform.rotation = Quaternion.Slerp(hasGravity[i].transform.rotation, targetRotation, 2 * Time.deltaTime);
 
                     Vector3 forceDown = downVec * multiplier;
 
 
 
                     hasGravity[i].GetComponent<Rigidbody>().AddForce(forceVector * forceMagnitude);
+                }
+
+                else
+                {
+
+
+                    Vector3 downVec = playerPlanet.transform.position - hasGravity[i].transform.position;
+
+                  //  float forceMagnitude = multiplier * grav / downVec.sqrMagnitude;
+                   // Vector3 forceVector = downVec.normalized;
+
+
+                    Vector3 bodyUp = hasGravity[i].transform.up;
+
+
+                    Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, downVec/grav) * hasGravity[i].transform.rotation;
+                    hasGravity[i].transform.rotation = Quaternion.Slerp(hasGravity[i].transform.rotation, targetRotation, 2 * Time.deltaTime);
+
+                   // Vector3 forceDown = downVec * multiplier;
+
+
+
+                   // hasGravity[i].GetComponent<Rigidbody>().AddForce(downVec/grav);
                 }
             }
         }
