7f5f52e03072f9081b739305104a25876b62eec2
Tiffany Li <yitongl@andrew.cmu.edu>
2018-11-16 16:47:59 -0500

    Scaled gravity with size of planet

--- Assets/Script/GravManager.cs
+++ Assets/Script/GravManager.cs
@@ -2,85 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class GravManager : MonoBehaviour
-    {
+public class GravManager : MonoBehaviour
+{
+
     public GameObject playerPlanet;
-        public float grav = 9;
+    public float grav = 50;
     public bool playerGravOn = false;
-        public float backUpGrav;
-    public float multiplier = 1;
     public GameObject cameraCube;
     public List<GameObject> hasGravity = new List<GameObject>();
 
-    public GameObject grava;
-    //public bool gravOn = false;
-        // Use this for initialization
-        void Start()
+    private float multiplier = 4;
+
+    void Start()
+    {
+        multiplier *= Mathf.Pow(playerPlanet.transform.localScale.x, 2);      
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("hasPhysics"))
         {
-            backUpGrav = grav;
-        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("hasPhysics")){
             hasGravity.Add(obj.gameObject);
         }
-
-        }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-
-
-        // GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-
-
-
-
-
-        print("OK");
-
-
-
-
-
         if (playerGravOn == true)
         {
-
-
             for (int i = 0; i < hasGravity.Count; i++)
             {
-
-               
-                    //grav = 1;
                 Vector3 downVec = playerPlanet.transform.position - hasGravity[i].transform.position;
-
                 float forceMagnitude = multiplier * grav / downVec.sqrMagnitude;
                 Vector3 forceVector = downVec.normalized;
 
-
                 Vector3 bodyUp = hasGravity[i].transform.up;
 
-
-                Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, forceMagnitude * forceVector) * hasGravity[i].transform.rotation;
+                Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, forceMagnitude * downVec) * hasGravity[i].transform.rotation;
                 hasGravity[i].transform.rotation = Quaternion.Slerp(hasGravity[i].transform.rotation, targetRotation, 2 * Time.deltaTime);
-
 
                 if (hasGravity[i].name != "CameraCube")
                 {
 
                     Vector3 forceDown = downVec * multiplier;
-
-
-
                     hasGravity[i].GetComponent<Rigidbody>().AddForce(forceVector * forceMagnitude);
                 }
             }
         }
-
-                
-
-
-            }
-        }
+    }
+}
     
 
 
