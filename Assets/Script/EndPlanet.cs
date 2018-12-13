0c353c6299ebd9f0ff17f59037b417a6e75f282f
Tiffany Li <yitongl@andrew.cmu.edu>
2018-12-12 19:23:29 -0500

    Added end planet script and restart button

---
+++ Assets/Script/EndPlanet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndPlanet : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject restartScreen;
+
+    private bool showRestart = false;
+
+	// Use this for initialization
+	void Start () {
+        if (restartScreen != null)
+            restartScreen.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (showRestart && restartScreen != null)
+            restartScreen.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            showRestart = true;
+        }
+    }
+}
